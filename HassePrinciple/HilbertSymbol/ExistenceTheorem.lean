43ffbe5387d6b970e08ed2a475bb792fe850afcd
NirvanaC93 <58878663+NirvanaC93@users.noreply.github.com>
2026-06-16 00:56:58 +0200

    update ExistenceTheorem

--- HassePrinciple/HilbertSymbol/ExistenceTheorem.lean
+++ HassePrinciple/HilbertSymbol/ExistenceTheorem.lean
@@ -15,7 +15,6 @@ public import HassePrinciple.NumberTheory.ApproximationTheorem
 
 namespace hilbertSym
 
-
 /-- Given a finite set of rational numbers `{a_i}_{i ∈ I}` and numbers `e_{i,v} ∈ {± 1}`,
 there exists a rational number `x` such that the Hilbert symbols `(x,a_i)_v` at each place `v`
 is equal to `e_{i,v}` if and only if
@@ -32,6 +31,82 @@ theorem exists_rat_with_prescribed_hilbert_symbols_at_finitely_many_places
           (einf i * ∏ᶠ (p : Nat.Primes), efin (i, p) = 1) ∧
           ((∀ (p : Nat.Primes), ∃ xp : ℚ_[p], efin (i, p) = hilbertSym xp (a i)) ∧
             ∃ xr : ℝ, einf i = hilbertSym xr (a i)) := by
-  sorry
+  constructor
+  · intro ⟨x, hx⟩ i
+    specialize hx i
+    constructor
+    · suffices {(p : Nat.Primes) | ¬atP x (a i) p = 1} = {(p : Nat.Primes) | ¬efin (i, p) = 1} by
+        simp only [Filter.eventually_cofinite]
+        rw [← this]
+        exact almost_all_one x (a i : ℚˣ)
+      ext n
+      simp only [Set.mem_setOf_eq, ← hx.1]
+    · constructor
+      · rw [← prod_eq_one x (a i), ← hx.2]
+        simp only [mul_eq_mul_left_iff]
+        left
+        congr
+        simp only [hx]
+      · constructor
+        · intro n
+          use x
+          rw [hx.1 n]
+        · use x
+          rw [hx.2]
+  · revert a
+    suffices ∀ (a : I → ℚˣ), (∀ i : I, ∃ bi : ℤ, (a i).val = bi) → (∀ (i : I),
+      (∀ᶠ (p : Nat.Primes) in Filter.cofinite, efin (i, p) = 1) ∧
+        einf i * ∏ᶠ (p : Nat.Primes), efin (i, p) = 1 ∧
+          (∀ (p : Nat.Primes), ∃ xp, efin (i, p) = hilbertSym xp (a i).val) ∧
+            ∃ xr, einf i = hilbertSym xr (a i).val) →
+              ∃ x, ∀ (i : I), (∀ (p : Nat.Primes), efin (i, p) = atP x (a i) p) ∧
+                einf i = atInfty x (a i) by
+      intro a
+      have int_repr : ∀ (i : I), ∃ (bi ci : ℤ), (a i).val * ci^2 = bi := by
+        intro i
+        use (a i).val.num * (a i).val.den
+        use (a i).val.den
+        nth_rw 1 [← Rat.num_div_den (a i).val]
+        field_simp
+        simp only [Int.cast_natCast, Int.cast_mul]
+        ring_nf
+
+      sorry
+    sorry
+
+
+    -- have int_repr : ∀ (i : I), ∃ (bi ci : ℤ), (a i).val * ci^2 = bi := by
+    --   intro i
+    --   use (a i).val.num * (a i).val.den
+    --   use (a i).val.den
+    --   nth_rw 1 [← Rat.num_div_den (a i).val]
+    --   field_simp
+    --   simp only [Int.cast_natCast, Int.cast_mul]
+    --   ring_nf
+    -- have padic_red_to_int (p : Nat.Primes) : ∀ x : ℚ_[p], ∀ (i : I), ∃ (bi ci : ℤ),
+    --     hilbertSym x (a i) = hilbertSym x bi := by
+    --   intro x i
+    --   have ⟨bi, ci, repi⟩ := int_repr i
+    --   use bi
+    --   use ci
+    --   have : hilbertSym x ((a i).val * ci^2) = hilbertSym x (a i).val := by
+    --     nth_rw 1 [(by ring_nf : x = x * 1^2), mul_square_eq]
+    --   rw [← this]
+    --   congr
+    --   exact_mod_cast repi
+    -- have real_red_to_int : ∀ x : ℝ, ∀ (i : I), ∃ (bi ci : ℤ),
+    --     hilbertSym x (a i) = hilbertSym x bi := by
+    --   intro x i
+    --   have ⟨bi, ci, repi⟩ := int_repr i
+    --   use bi
+    --   use ci
+    --   have : hilbertSym x ((a i).val * ci^2) = hilbertSym x (a i).val := by
+    --     nth_rw 1 [(by ring_nf : x = x * 1^2), mul_square_eq]
+    --   rw [← this]
+    --   congr
+    --   exact_mod_cast repi
+    -- intro h
+
+    -- sorry
 
 end hilbertSym
