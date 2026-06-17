aafac70c056dcc3e483124d7022597d96cd477a8
cmhsu2012 <73784462+cmhsu2012@users.noreply.github.com>
2026-06-17 11:42:58 -0600

    updated legendre lemmas after queen review

--- HassePrinciple/Padics/Legendre.lean
+++ HassePrinciple/Padics/Legendre.lean
@@ -17,12 +17,25 @@ namespace PadicInt
 
 variable {p : ℕ} [Fact (Nat.Prime p)]
 
+-- TODO: PR to Mathlib.NumberTheory.Padics.RingHoms
+@[simp]
+lemma zmodRepr_one : zmodRepr (1 : ℤ_[p]) = 1 := by
+  rw [← PadicInt.val_toZMod_eq_zmodRepr, map_one, ZMod.val_one p]
+
 /-- The zmodRepr of a p-adic unit is nonzero modulo p. -/
 lemma zmodRepr_coe_ne_zero_of_isUnit {a : ℤ_[p]} (ha : IsUnit a) :
     ((a.zmodRepr : ℤ) : ZMod p) ≠ 0 := by
   rw_mod_cast [ZMod.natCast_eq_zero_iff]
   exact Nat.not_dvd_of_pos_of_lt
     (Nat.ne_zero_iff_zero_lt.mp (PadicInt.zmodRepr_units_ne_zero ha.unit)) (zmodRepr_lt_p a)
+
+/-- Let `a` be a p-adic integer. It is a unit if and only if its zmodRepr is nonzero modulo p. -/
+lemma zmodRepr_coe_ne_zero_iff_isUnit {a : ℤ_[p]} :
+    IsUnit a ↔ ((a.zmodRepr : ℤ) : ZMod p) ≠ 0 := by
+  refine ⟨zmodRepr_coe_ne_zero_of_isUnit, fun h ↦ ?_⟩
+  rw [PadicInt.isUnit_iff, ← PadicInt.norm_natCast_zmodRepr_eq_one_iff,
+    PadicInt.norm_natCast_zmodRepr_eq_one_iff_ne]
+  aesop
 
 /-- We define the Legendre symbol for a `p`-adic integer `a` as the Legendre symbol (at `p`) of its
 reduction modulo `p`. -/
@@ -32,31 +45,16 @@ variable {a b : ℤ_[p]}
 namespace legendreSym
 
 lemma legendreSymMod (z : ℤ_[p]) : legendreSym (z) = legendreSym (z.zmodRepr : ℤ_[p]) := by
-  unfold legendreSym
-  unfold _root_.legendreSym
-  simp
+  simp [legendreSym, _root_.legendreSym]
 
 /-- The Padic Legendre symbol agrees with the classical Legendre symbol on ℕ. -/
 lemma intCastNat (z : ℕ) : legendreSym (z : ℤ_[p]) = _root_.legendreSym p z := by
-  unfold legendreSym
-  unfold _root_.legendreSym
-  have h₁ : (z : ZMod p)= (((z : ℤ_[p]).zmodRepr : ℤ): ZMod p) :=by
-    rw [zmodRepr_natCast]
-    simp
-  simp [quadraticCharFun, h₁]
+  simp [legendreSym, _root_.legendreSym, quadraticCharFun, zmodRepr_natCast]
 
 /-- The Padic Legendre symbol agrees with the classical Legendre symbol on ℤ. -/
 lemma intCast (z : ℤ) : legendreSym (z : ℤ_[p]) = _root_.legendreSym p z := by
-  let w := (z : ZMod p).val
-  have h₁ : _root_.legendreSym p z = _root_.legendreSym p w := by
-    rw [legendreSym.mod]
-    congr
-    exact Eq.symm (ZMod.val_intCast z)
-  have h₂ : legendreSym (z : ℤ_[p]) = legendreSym (w : ℤ_[p]) := by
-    rw [legendreSymMod]
-    congr
-    rw [← val_toZMod_eq_zmodRepr, map_intCast]
-  rw [h₁, h₂, intCastNat]
+   rw [legendreSym.mod, ← ZMod.val_intCast, legendreSymMod, ← val_toZMod_eq_zmodRepr, map_intCast,
+    intCastNat]
 
 /-- We have the congruence `legendreSym a ≡ a ^ (p / 2) mod p`. -/
 theorem eq_pow : (legendreSym a : ZMod p) = ((a.zmodRepr) : ZMod p) ^ (p / 2) := by
@@ -70,36 +68,23 @@ theorem eq_one_or_neg_one (ha : IsUnit a) : legendreSym a = 1 ∨ legendreSym a 
 /-- If a is a p-adic unit, then `legendreSym a = -1` iff `legendreSym a ≠ 1`. -/
 theorem eq_neg_one_iff_not_one (ha : IsUnit a) :
     legendreSym a = -1 ↔ ¬legendreSym a = 1 := by
-  constructor
-  · lia
-  · have := eq_one_or_neg_one ha
-    intro h
-    tauto
+  rw [legendreSym]
+  exact _root_.legendreSym.eq_neg_one_iff_not_one p (zmodRepr_coe_ne_zero_of_isUnit ha)
 
 /-- The Legendre symbol of `p` and `a` is zero iff `p ∣ a`. -/
 theorem eq_zero_iff : legendreSym a = 0 ↔ ¬ IsUnit a := by
-  constructor
-  · contrapose
-    intro h
-    have := eq_one_or_neg_one h
-    lia
-  · intro h
-    rw [not_isUnit_iff, norm_lt_one_iff_dvd] at h
-    unfold legendreSym
-    rw [_root_.legendreSym.eq_zero_iff, zmodRepr_eq_zero_of_dvd h]
-    simp
+    rw [legendreSym, ← not_iff_not, not_not, _root_.legendreSym.eq_zero_iff,
+    zmodRepr_coe_ne_zero_iff_isUnit]
 
 /-- The Legendre symbol at zero is zero. -/
 @[simp]
 theorem at_zero : legendreSym (0 : ℤ_[p]) = 0 := by
-  rw [← _root_.legendreSym.at_zero p]
-  apply intCast
+  simp [legendreSym, _root_.legendreSym.at_zero p]
 
 /-- The Legendre symbol at 1 is 1. -/
 @[simp]
 theorem at_one : legendreSym (1 : ℤ_[p]) = 1 := by
-  rw [← _root_.legendreSym.at_one p]
-  apply intCast
+  simp [legendreSym,  _root_.legendreSym.at_one]
 
 /-- The Legendre symbol is multiplicative in `a` for `p` fixed. -/
 protected theorem mul : legendreSym (a * b) = legendreSym a * legendreSym b := by
@@ -115,22 +100,12 @@ noncomputable def hom : ℤ_[p] →*₀ ℤ where
 
 /-- The square of the symbol is 1 if `a` is a unit. -/
 theorem sq_one (ha : IsUnit a) : legendreSym a ^ 2 = 1 := by
-  cases eq_one_or_neg_one ha
-  all_goals {
-    expose_names
-    rw [h]
-    ring
-    }
+   cases eq_one_or_neg_one ha <;> aesop
 
 /-- The Legendre symbol of `a^2` at `p` is 1 if `a` is a unit. -/
 theorem sq_one' (ha : IsUnit a) : legendreSym (a ^ 2) = 1 := by
   rw [pow_two, legendreSym.mul]
-  cases eq_one_or_neg_one ha
-  all_goals {
-    expose_names
-    rw [h]
-    ring
-    }
+  cases eq_one_or_neg_one ha <;> aesop
 
 /-- If `a` is a unit, then `legendreSym a = 1` iff `a` is a square mod `p`. -/
 theorem eq_one_iff (ha : IsUnit a) : legendreSym a = 1 ↔ IsSquare ((a.zmodRepr : ℤ) : ZMod p) := by
