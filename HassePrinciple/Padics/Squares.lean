a3fb0597dee368cb61a9d5185ee557a8224fba06
mariainesdff <mariaines.dff@gmail.com>
2026-07-22 11:58:58 +0200

    add Lean proof

--- HassePrinciple/Padics/Squares.lean
+++ HassePrinciple/Padics/Squares.lean
@@ -6,34 +6,13 @@ Authors: Nirvana Coppola, María Inés de Frutos-Fernández
 
 module
 
-public import Mathlib.NumberTheory.Padics.PadicNumbers
-public import Mathlib.Topology.Algebra.OpenSubgroup
-
-public import Mathlib.NumberTheory.Padics.RingHoms
-public import Mathlib.Topology.Algebra.IsOpenUnits
-public import Mathlib.Algebra.Group.Pointwise.Set.Scalar
+public import HassePrinciple.Padics.Lemmas
 public import Mathlib.Analysis.Normed.Module.Ball.Pointwise
-
+public import Mathlib.Topology.Algebra.IsOpenUnits
 
 /-! # Padic unit squares form an open subgroup. -/
 
 @[expose] public section
-
-namespace PadicInt
-
-open Polynomial
-
-/-- An element in ℤ_p (p odd) is a square if its reduction modulo p is a square. -/
-lemma isSquare_of_zmod {p : ℕ} [Fact (Nat.Prime p)] (hodd : p ≠ 2) {m : ℤ_[p]}
-    (hmod : IsSquare m.toZMod) : IsSquare m := by
-  let F : ℤ_[p][X] := X ^ 2 - C m
-  sorry
-
-/-- An element in ℤ_2 is a square if its reduction modulo 8 is a square. -/
-lemma isSquare_of_zmodPow {m : ℤ_[2]} (hmod : IsSquare (m.toZModPow 3)) : IsSquare m := by sorry
-
-end PadicInt
-
 namespace Padic
 
 variable (p : ℕ) [Fact (Nat.Prime p)]
@@ -46,36 +25,61 @@ lemma isSquare_of_dist_one_lt_one {p : ℕ} [Fact (Nat.Prime p)] (hp : p ≠ 2) 
     ← Nat.cast_one, PadicInt.norm_le_pow_iff_mem_span_pow, pow_one, Ideal.mem_span_singleton] at hx
   have hz1 : IsSquare (z + 1) := by
     have hz0 : PadicInt.toZMod z = 0 := by
-      rw [← RingHom.mem_ker, PadicInt.ker_toZMod, PadicInt.maximalIdeal_eq_span_p]
-      simp [Ideal.mem_span_singleton, hx]
-    exact (z + 1).isSquare_of_zmod hp (by simp [hz0])
+      simp [← RingHom.mem_ker, PadicInt.ker_toZMod, PadicInt.maximalIdeal_eq_span_p,
+        Ideal.mem_span_singleton, hx]
+    exact (z + 1).isSquare_of_zmod hp (by simp [dvd_add_right hx, ← PadicInt.norm_lt_one_iff_dvd])
+      (by simp [hz0])
   obtain ⟨r, hr⟩ := hz1
   exact ⟨r, by simp [← PadicInt.coe_mul, ← hr, z]⟩
 
-open Pointwise
+lemma isSquare_of_dist_one_lt_pow {x : ℚ_[2]} (hx : dist x 1 < 2 ^ (-(2 : ℤ))) : IsSquare x := by
+  have hx1 : ‖x - 1‖ ≤ 1 := le_trans hx.le (by norm_num)
+  let z : ℤ_[2] := ⟨x - 1, hx1⟩
+  have : ‖x - 1‖ = ‖z‖ := by simp [z]
+  rw [dist_eq_norm, ← Nat.cast_two (R := ℝ), norm_lt_pow_iff_norm_le_pow_sub_one, this] at hx
+  simp only [ Int.reduceSub, ← Nat.cast_three (R := ℤ), PadicInt.norm_le_pow_iff_mem_span_pow,
+    Ideal.mem_span_singleton] at hx
+  rw [Nat.cast_two] at hx
+  have hz1 : IsSquare (z + 1) := by
+    have hz0 : z.toZModPow 3 = 0 := by
+      simp [-Nat.reducePow, ← RingHom.mem_ker, PadicInt.ker_toZModPow, Ideal.mem_span_singleton, hx]
+    refine (z + 1).isSquare_of_zmodPow ?_ (by simp [hz0])
+    rw [dvd_add_right (dvd_trans (dvd_pow_self 2 three_ne_zero) hx), ← Nat.cast_two (R := ℤ_[2]),
+      ← PadicInt.norm_lt_one_iff_dvd]
+    simp
+  obtain ⟨r, hr⟩ := hz1
+  exact ⟨r, by simp [← PadicInt.coe_mul, ← hr, z]⟩
+
+lemma exists_pow_isSquare_of_dist_one_lt (p : ℕ) [Fact (Nat.Prime p)] :
+    ∃ (n : ℕ), ∀ (x : ℚ_[p]), dist x 1 < p ^ (-(n : ℤ)) → IsSquare x := by
+  by_cases hp : p = 2
+  · subst hp
+    exact ⟨2, fun x ↦ isSquare_of_dist_one_lt_pow⟩
+  · exact ⟨0, fun x ↦ isSquare_of_dist_one_lt_one hp⟩
+
+open Pointwise Set
 
 lemma isOpen_squares_sdiff_zero : IsOpen ({x : ℚ_[p] | IsSquare x} \ {0}) := by
-  refine isOpen_iff_forall_mem_open.mpr ?_
+  rw [isOpen_iff_forall_mem_open]
   intro x ⟨hx, hx0⟩
-  simp only [Set.mem_setOf_eq, Set.mem_singleton_iff] at hx hx0
-  by_cases hp : p = 2
-  · sorry
-  · use x • (Metric.ball (1 : ℚ_[p]) 1)
-    refine ⟨?_, by simp [smul_ball hx0, Metric.isOpen_ball],
-      Set.mem_smul_set.mpr ⟨1, by simp, by simp⟩⟩
-    intro y hy
-    simp only [← Set.image_smul, Set.mem_image, Metric.mem_ball, Set.mem_sdiff, Set.mem_setOf_eq,
-      Set.mem_singleton_iff] at hy ⊢
-    obtain ⟨u, hup, hu⟩ := hy
-    simp only [← hu, smul_eq_mul, mul_eq_zero, not_or]
-    refine ⟨hx.mul (isSquare_of_dist_one_lt_one hp hup), hx0, ?_⟩
-    · sorry
+  simp only [mem_setOf_eq, mem_singleton_iff] at hx hx0
+  obtain ⟨n, hn⟩ := exists_pow_isSquare_of_dist_one_lt p
+  refine ⟨x • (Metric.ball (1 : ℚ_[p]) (p ^ (-(n : ℤ)))), fun y hy ↦ ?_, by
+    simp [smul_ball hx0, Metric.isOpen_ball], mem_smul_set.mpr ⟨1,
+      Metric.mem_ball_self (zpow_pos (Nat.cast_pos.mpr (Nat.pos_of_neZero p)) _), by simp⟩⟩
+  simp only [← image_smul, mem_image, Metric.mem_ball, mem_sdiff, mem_setOf_eq,
+    mem_singleton_iff] at hy ⊢
+  obtain ⟨u, hup, hu⟩ := hy
+  simp only [← hu, smul_eq_mul, mul_eq_zero, not_or]
+  refine ⟨hx.mul (hn u hup), hx0, ?_⟩
+  by_contra h0
+  simp only [h0, dist_zero, norm_one, ← zpow_zero (p : ℝ)] at hup
+  rw [zpow_lt_zpow_iff_right₀ (Nat.one_lt_cast.mpr (Nat.Prime.one_lt Fact.out))] at hup
+  simp [← not_le] at hup
 
-
---TODO: rename to unitSquares
 -- Proven in section II.3.3.
 /-- The open subgroup of `p`-adic unit squares inside `ℚ_[p]ˣ`. -/
-def squares : OpenSubgroup ℚ_[p]ˣ where
+def unitSquares : OpenSubgroup ℚ_[p]ˣ where
   carrier              := {x : ℚ_[p]ˣ | IsSquare x}
   mul_mem' {x y} hx hy := IsSquare.mul hx hy
   one_mem'             := by simp
@@ -83,7 +87,7 @@ def squares : OpenSubgroup ℚ_[p]ˣ where
   isOpen'              := by
     have h : (Units.val '' {x : ℚ_[p]ˣ | IsSquare x}) = {x : ℚ_[p] | IsSquare x} \ {0} := by
       ext x
-      simp only [Set.mem_image, Set.mem_setOf_eq, Set.mem_sdiff, Set.mem_singleton_iff]
+      simp only [mem_image, mem_setOf_eq, mem_sdiff, mem_singleton_iff]
       refine ⟨fun ⟨u, hu2, hux⟩ ↦ ?_, fun ⟨hxs, hx0⟩ ↦ ?_⟩
       · simp only [← hux, Units.ne_zero, not_false_eq_true, and_true]
         obtain ⟨v, hv⟩ := hu2
